fa5195fc90417163986707d460d2271ca9a09eb9
Sushhi-cdr <enzoseiti@gmail.com>
2026-03-16 16:17:43 -0300

    Criação do Services

---
+++ src/main/java/com/example/Ex3/Services/ServicoPedido.java
@@ -0,0 +1,31 @@
+package com.example.Ex3.Services;
+
+import com.example.Ex3.Entity.Pedido;
+import com.example.Ex3.Repositories.RepositorioPedido;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServicoPedido {
+
+    @Autowired
+    private RepositorioPedido repositorio;
+
+    public Pedido criar(Pedido pedido) {
+        return repositorio.save(pedido);
+    }
+
+    public List<Pedido> listar(){
+        return repositorio.findAll();
+    }
+
+    public Pedido buscarPorID(Long id){
+        return repositorio.findById(id).orElse(null);
+    }
+
+    public void excluir(Pedido pedido){
+        repositorio.deleteById(pedido.getId());
+    }
+}
